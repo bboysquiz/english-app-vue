e62cbac40985d6481e35ba89b870de5f50116538
bboysquiz <mrkiraller@gmail.com>
2024-12-21 15:56:03 +0300

    fix proxy

--- vite.config.js
+++ vite.config.js
@@ -7,18 +7,23 @@ export default defineConfig({
   server: {
     proxy: {
       '/api': {
-        target: 'https://english-app-vue-backend-2.onrender.com',
+        target: 'https://english-app-vue-backend-2.onrender.com/api',
         changeOrigin: true,
-        rewrite: (path) => path.replace(/^\/api/, ''), // Удаляет `/api` из пути
+        rewrite: (path) => path.replace(/^\/api/, ''),
+        configure: (proxy, options) => {
+          proxy.on('proxyReq', (proxyReq, req, res) => {
+            console.log(`[PROXY] ${req.method} ${req.url} -> ${proxyReq.path}`);
+          });
+        },
       },
     },
-  },
-  build: {
-    rollupOptions: {
-      output: {
-        assetFileNames: 'assets/[name]-[hash][extname]',
-        entryFileNames: 'assets/[name]-[hash].js',
+    build: {
+      rollupOptions: {
+        output: {
+          assetFileNames: 'assets/[name]-[hash][extname]',
+          entryFileNames: 'assets/[name]-[hash].js',
+        },
       },
     },
-  },
+  }
 })
