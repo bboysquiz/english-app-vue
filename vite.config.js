6368eabdcc3e92f97ce7cf7d0ef70ae5a3803b76
bboysquiz <mrkiraller@gmail.com>
2024-12-21 17:24:29 +0300

    fix: fixed proxy

--- vite.config.js
+++ vite.config.js
@@ -17,13 +17,14 @@ export default defineConfig({
         },
       },
     },
-    build: {
-      rollupOptions: {
-        output: {
-          assetFileNames: 'assets/[name]-[hash][extname]',
-          entryFileNames: 'assets/[name]-[hash].js',
-        },
+  },
+  build: {
+    rollupOptions: {
+      output: {
+        assetFileNames: 'assets/[name]-[hash][extname]',
+        entryFileNames: 'assets/[name]-[hash].js',
+        chunkFileNames: 'assets/[name]-[hash].js',
       },
     },
-  }
+  },
 })
